b2438d158e14f0236d113f355e9bf4840a33d5bf
Martin Klefas-Stennett <cleverbum@hotmail.com>
2023-03-01 21:37:46 +0000

    started putting in code - not functional

---
+++ main.py
@@ -0,0 +1,54 @@
+import os
+
+import numpy as np
+
+import tensorflow as tf
+assert tf.__version__.startswith('2')
+
+from tflite_model_maker import model_spec
+from tflite_model_maker import image_classifier
+from tflite_model_maker.config import ExportFormat
+from tflite_model_maker.config import QuantizationConfig
+from tflite_model_maker.image_classifier import DataLoader
+
+import matplotlib.pyplot as plt
+
+#image_path = os.path.join('./', 'categorized_photos')
+
+image_path = tf.keras.utils.get_file(
+      'flower_photos.tgz',
+      'https://storage.googleapis.com/download.tensorflow.org/example_images/flower_photos.tgz',
+      extract=True)
+image_path = os.path.join(os.path.dirname(image_path), 'flower_photos')
+
+data = DataLoader.from_folder(image_path)
+train_data, test_data = data.split(0.9)
+
+model = image_classifier.create(train_data)
+
+# A helper function that returns 'red'/'black' depending on if its two input
+# parameter matches or not.
+def get_label_color(val1, val2):
+  if val1 == val2:
+    return 'black'
+  else:
+    return 'red'
+
+# Then plot 100 test images and their predicted labels.
+# If a prediction result is different from the label provided label in "test"
+# dataset, we will highlight it in red color.
+plt.figure(figsize=(20, 20))
+predicts = model.predict_top_k(test_data)
+for i, (image, label) in enumerate(test_data.gen_dataset().unbatch().take(100)):
+  ax = plt.subplot(10, 10, i+1)
+  plt.xticks([])
+  plt.yticks([])
+  plt.grid(False)
+  plt.imshow(image.numpy(), cmap=plt.cm.gray)
+
+  predict_label = predicts[i][0][0]
+  color = get_label_color(predict_label,
+                          test_data.index_to_label[label.numpy()])
+  ax.xaxis.label.set_color(color)
+  plt.xlabel('Predicted: %s' % predict_label)
+plt.show()
